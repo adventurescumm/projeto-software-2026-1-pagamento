f8aed042f6384551650f3c8e7d91db337519290f
adventurescumm <gab05mas@gmail.com>
2026-03-25 11:45:38 -0300

    modelo modificado

--- models.py
+++ models.py
@@ -2,14 +2,7 @@ import uuid
 from db import db
 
 class Pagamento(db.Model):
-    __tablename__ = "post"
+    __tablename__ = "pagamento"
 
-    id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    codigo = db.Column(db.String(100), nullable=False, unique=False)
-    valor_total = db.Column(db.Integer, nullable=False, unique=False)
-    tipo_pagamento = db.Column(db.String(120), nullable=False, unique=False)
-    numero_parcelas = db.Column(db.Integer, nullable=False)
-    valor_parcela = db.Column(db.Integer, nullable=False)
-    data = db.Column(db.DateTime)
-    id_usuario = db.Column(db.String(120), nullable=False)
+    id_usuario = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     email = db.Column(db.String(120), nullable=False, unique=True)
